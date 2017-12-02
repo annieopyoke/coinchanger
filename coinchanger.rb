038ca3d02b657bbd043db2e54962489e47041611
annieopyoke <annieopyoke@gmail.com>
2017-12-02 15:26:38 -0500

    nickel test pass

--- coinchanger.rb
+++ coinchanger.rb
@@ -4,9 +4,15 @@
 
 def change_maker(change)
 	coins = {}
-	if change > 0
+	if change > 0 && change < 5
 	# change, penny,amount of pennys
 	coins[:penny] = change 
+elsif change > 4 
+	coins[:nickel] = change / 5
+	change = change % 5
+	if change > 0
+		coins[:penny] = change
+	end
 end
 	coins
 	
